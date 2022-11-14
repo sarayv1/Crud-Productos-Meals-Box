9e316027d488a53937e5751dda267e52ad8249db
sarayv1 <sarayqvsao@gmail.com>
2022-11-14 18:19:47 -0500

    crear

---
+++ 01-hola.py
@@ -0,0 +1,9 @@
+print("Hola mundo")
+apple="manzanas"
+print (apple)
+
+#para concatenar en python se utiliza la coma ,
+print("Esta es la manzana",547)dd.
+#esto es un comentario para solo una linea de codigo
+
+"""esto es otra forma de hacer un comentario pero para comentar codeigo por bloques"""
